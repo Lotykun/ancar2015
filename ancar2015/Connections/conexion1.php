d0cefa670d5ba387f93e8e09cce6d07e94bf13bf
Alejandro <alejandrodob@gmail.com>
2014-12-07 17:56:43 +0100

    Disponibilidad otros jugadores en el email de aviso

--- ancar2015/Connections/conexion1.php
+++ ancar2015/Connections/conexion1.php
@@ -272,7 +272,6 @@ function getFaltanJugvotar($idpartido){
             GetSQLValueString($idpartido, "int"));
 
     $reg = mysql_query($query, $conexion1);
-    /*$row_reg = mysql_fetch_assoc($reg);*/
     $resultado = mysql_num_rows($reg);
 
     return $resultado;
@@ -295,6 +294,23 @@ function getMVP($idpartido){
     $reg = mysql_query($query, $conexion1);
     $row_reg = mysql_fetch_assoc($reg);
     $resultado = $row_reg['idjugador'];
+
+    return $resultado;
+}
+function getDisponibilidadOtrosJugadores($idpartido, $idjugador){
+    $conexion1=conectarBBDD();
+
+    $query = sprintf("SELECT jugador.nombre, jugador.apellidos, jugador.dorsal, disp.disponible
+                    FROM `relacion_jug_disponibilidad` AS disp, `jugador`
+                    WHERE jugador.jugador_id=disp.idjugador
+                    AND disp.idpartido=%s
+                    AND jugador.jugador_id!=%s
+                    ORDER BY disp.disponible DESC",
+                    GetSQLValueString($idpartido, "int"),
+                    GetSQLValueString($idjugador, "int"));
+
+    $reg = mysql_query($query, $conexion1);
+    $resultado = mysql_fetch_assoc($reg);
 
     return $resultado;
 }
